c58e5652e94342a2ad396b50cbe65c1d7680c58d
kimkyeonghye <guuhwi@gmail.com>
2022-04-05 21:13:08 +0900

    array, object, function

---
+++ function.js
@@ -0,0 +1,7 @@
+function sayHello(nameOfPerson, age){
+    console.log("Hello My name is " + nameOfPerson + " and I'm " + age);
+}
+
+sayHello('nico', 10);
+sayHello('dal', 23);
+sayHello('lynn', 21);
---
+++ objects.js
@@ -0,0 +1,11 @@
+const player = {
+    name: 'nico',
+    points: 10,
+    fat: true,
+};
+
+console.log(player.name);
+console.log(player['points']);
+
+// Add
+player.lastName = 'potato';
