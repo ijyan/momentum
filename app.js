57fef2e792ff72f24e28c4b3d80f9fafd524ec97
kimkyeonghye <guuhwi@gmail.com>
2022-04-14 21:30:13 +0900

    4.4 Getting Username

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const loginForm = document.querySelector("#login-form")
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+// string만 포함된 변수는 대문자로 표기, string을 저장하고 싶을 때 사용, 중요한 정보를 담은게 아님.
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
     // preventDefault : 브라우저가 기본 동작을 실행하지 못하게 막기
     event.preventDefault();
-    console.log(loginInput.value);
-}
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const userName = loginInput.value;
+    //greeting.innerText = "Hello " + userName;
+    greeting.innerText = `Hello ${userName}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 
-function handleLinkClick(event) {
-    event.preventDefault();
-    console.dir(event);
+    console.log(userName);
 }
 
 // addEventListenr 안에 있는 함수는 직접 실행( ex : handleLinkClick() )하지 않아야한다.
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
