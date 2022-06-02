23526aea1449683129c29ca2733459c92d6fe42f
kimkyeonghye <guuhwi@gmail.com>
2022-06-02 21:44:35 +0900

    #5.2 padStart

--- js/clock.js
+++ js/clock.js
@@ -2,7 +2,12 @@ const clock = document.querySelector('.clock');
 
 function getClock() {
     const date = new Date();
-    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+
+    // date.getHours() 를 string으로 변환환
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
 // 즉시 호출 : website가 load 되자마자 getClock()을 바로 실행하기 위해
