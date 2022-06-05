daa145e14ba0896acb0b5840e70b75b0f083919d
kimkyeonghye <guuhwi@gmail.com>
2022-06-05 20:03:11 +0900

    #8.1 weather - 해당 위치의 날씨 알려줌

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,20 @@
+const API_KEY = '3094f59490e5027536d436d0b9f4ed99';
+
 function onGeoSuccess(position) {
     let lat = position.coords.latitude;
-    let lng = position.coords.longitude;
-    console.log('you live in', lat, lng);
+    let lon = position.coords.longitude;
+    // &units=metric : 섭씨 온도
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            const weather = document.querySelector('.weather span:first-child');
+            const city = document.querySelector('.weather span:last-child');
+            const name = data.name;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+            city.innerText = data.name;
+        });
 }
 
 function onGeoError() {
