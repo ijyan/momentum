d0d93e0f149bc1c639131a9502a7e0c739610ffd
kimkyeonghye <guuhwi@gmail.com>
2022-06-05 17:26:28 +0900

    #7.7 todo 객체로 바꿈

--- js/todo.js
+++ js/todo.js
@@ -18,8 +18,10 @@ function deleteToDo(event) {
 
 function paintToDo(newToDo) {
     const list = document.createElement('li');
+    list.id = newToDo.id;
     const span = document.createElement('span');
-    span.innerText = newToDo;
+    // newTodoObj의 text 대입
+    span.innerText = newToDo.text;
     const button = document.createElement('button');
     button.innerText = '❌';
     button.addEventListener('click', deleteToDo);
@@ -30,11 +32,14 @@ function paintToDo(newToDo) {
 
 function handleToDoSubmit(event) {
     event.preventDefault();
-    // input의 현재 value를 새로운 변수에 대입
     const newToDo = toDoInput.value;
     toDoInput.value = '';
-    toDos.push(newToDo);
-    paintToDo(newToDo);
+    const newTodoObj = {
+        text: newToDo,
+        id: Date.now(),
+    };
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj);
     saveToDos();
 }
 
@@ -45,8 +50,5 @@ const savedToDos = localStorage.getItem(TODOS_KEY);
 if (savedToDos !== null) {
     const parsedToDos = JSON.parse(savedToDos);
     toDos = parsedToDos;
-    // forEach: array에 있는 각각의 item에 대해서 function을 실행하게 해준다
     parsedToDos.forEach(paintToDo);
-    // function을 만들지 않고 사용할 때
-    // parsedToDos.forEach((item) => console.log('this is the turn of ', item));
 }
