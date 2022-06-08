6159a653fce0e7d6df80ee967ac0d9d0dac4a83d
kimkyeonghye <guuhwi@gmail.com>
2022-06-08 23:08:16 +0900

    시간 업데이트

--- js/clock.js
+++ js/clock.js
@@ -1,8 +1,8 @@
 const currentDate = document.querySelector('.current__date');
 const currentClock = document.querySelector('.current__clock');
-const date = new Date();
 
 function getClock() {
+    const date = new Date();
     const hours = String(date.getHours()).padStart(2, '0');
     const minutes = String(date.getMinutes()).padStart(2, '0');
     const seconds = String(date.getSeconds()).padStart(2, '0');
@@ -10,13 +10,13 @@ function getClock() {
 }
 
 function getDate() {
+    const date = new Date();
     const dayName = date.toLocaleString('en-US', { weekday: 'short' });
     const day = String(date.getDate());
     const month = String(date.getMonth() + 1);
     currentDate.innerText = `${dayName}. ${month}월 ${day}일 `;
 }
 
-// 즉시 호출 : website가 load 되자마자 getClock()을 바로 실행하기 위해
 getClock();
 getDate();
 setInterval(getClock, 1000);
