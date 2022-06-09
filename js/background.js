770ae7c5a04f218f6b554b5cd8d565cbb100089a
kimkyeonghye <guuhwi@gmail.com>
2022-06-09 22:41:40 +0900

    bg main태그에 추가

--- js/background.js
+++ js/background.js
@@ -1,9 +1,12 @@
+const main = document.querySelector('#main');
 const images = ['bg_1.jpg', 'bg_2.jpg', 'bg_3.jpg'];
 
 const randomImage = images[Math.floor(Math.random() * images.length)];
 
-// img 태그
-const bgImage = document.createElement('img');
-bgImage.src = `img/${randomImage}`; // <img src='img/bg_1.jpg'>;
+function paintImg(randomImage) {
+    main.classList.add('bg');
+    const bg = document.querySelector('.bg');
+    bg.style.backgroundImage = `url(img/${randomImage})`;
+}
 
-document.body.appendChild(bgImage);
+paintImg(randomImage);
