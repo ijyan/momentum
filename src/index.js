63b203f4945eafe21eaabd305f965e2b3191e28b
kimkyeonghye <guuhwi@gmail.com>
2022-06-22 22:24:42 +0900

    package.json

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import background from './js/background.js';
+import clock from './js/clock.js';
+import greeting from './js/greetings.js';
+import quotes from './js/quotes.js';
+import todo from './js/todo.js';
+import weather from './js/weather.js';
+
+function init(){
+    background();
+    clock();
+    greeting();
+    quotes();
+    todo();
+    weather();
+}
+
+init();
